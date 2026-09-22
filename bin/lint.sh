56a60b0c60bc3b5021b07008cee289217adf8c46
Daniel <dcolinmorgan@gmail.com>
2026-09-22 15:28:52 +0200

    Stop CI creating its virtualenv inside the package directory
    Every job ran `python -m venv cu_cat`, which put bin/, lib/ and
    pyvenv.cfg inside the cu_cat package directory. `bin/lint.sh` then ran
    flake8 over that tree and failed on third-party code:
    
        cu_cat/lib/python3.8/site-packages/psutil/_common.py:38:1:
            F822 undefined name 'NIC_DUPLEX_FULL' in __all__
    
    which is why CI has been red regardless of the state of the source. The
    venv now lives in .venv, and lint.sh excludes site-packages, .venv and
    notebook checkpoints so a stray environment cannot fail the build again.

--- bin/lint.sh
+++ bin/lint.sh
@@ -18,6 +18,7 @@ flake8 --version
 # Deeper check
 flake8 \
   cu_cat \
+  --exclude='*/site-packages/*,.venv,*/.ipynb_checkpoints/*' \
   --count \
   --select=E9,F63,F7,F82 \
   --ignore=C901,E121,E122,E123,E124,E125,E128,E131,E144,E201,E202,E203,E231,E251,E265,E301,E302,E303,E401,E501,E722,F401,W291,W293,F821,E999 \
